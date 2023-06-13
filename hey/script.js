08f00278b77af20ea8fa96e9bbac8dc5194a2dda
Gagan Shenvi <107752760+imgagan18@users.noreply.github.com>
2023-06-13 16:42:39 +0530

    Add files via upload

---
+++ hey/script.js
@@ -0,0 +1,7 @@
+let username;
+
+document.getElementById("mybutton").onclick= function(){
+    username= getElementById("myText").values;
+    console.log(username);
+    document.getElementById("mylabel").innerHTML=username;
+}
